8bfd2c3a6748db61b5f04b0135296cd84bd4316d
domenicatonziello <domenica.tonziello10@gmail.com>
2023-01-19 15:33:21 +0100

    milestone 1

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  name: 'App',
+  components: {
+    AppTitle,
+  }
+}
+</script>
+
+<template>
+  <app-title></app-title>
+</template>
+
+
+<style>
+
+</style>
