a01f5cec1f6547f051df47ff1483510fef997149
domenicatonziello <domenica.tonziello10@gmail.com>
2023-01-19 15:48:11 +0100

    img dinamica

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 export default {
   name: 'App',
   components: {
     AppTitle,
+    AppHero,
   }
 }
 </script>
 
 <template>
   <app-title></app-title>
+  <app-hero></app-hero>
 </template>
 
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'AppHero',
+    data() {
+        return {
+            images: ['vue.svg', 'image-1.jpg'],
+        }
+    },
+    methods: {
+        buildImagePath(image) {
+            const url = new URL(`../assets/${image}`, import.meta.url)
+            return url.href;
+        }
+    }
+}
+</script>
+
+<template>
+    <img v-for="image in images" :src="buildImagePath(image)" :alt="image">
+</template>
+
+<style scoped>
+img {
+    max-width: 400px;
+    display: block;
+    margin: 10px 0;
+}
+</style>
